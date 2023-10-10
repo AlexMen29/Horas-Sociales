326f8930b065c64d9015d7d26a11e0a025769be4
Vladimir Ascencio <vladimirramos405@gmail.com>
2023-10-10 07:30:32 -0600

    Cambios XD

---
+++ SistemaAdministrativo/FrmInicio.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace SistemaAdministrativo
+{
+    partial class FrmSisAd
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmSisAd));
+            this.PanelOpciones = new System.Windows.Forms.Panel();
+            this.btnHojaSocial = new System.Windows.Forms.Button();
+            this.btnEnviarMensaje = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.btnDetalles = new System.Windows.Forms.Button();
+            this.btnInicio = new System.Windows.Forms.Button();
+            this.btnCerrarSesion = new System.Windows.Forms.Button();
+            this.PanelMenu = new System.Windows.Forms.Panel();
+            this.btnPerfil = new System.Windows.Forms.Button();
+            this.btnNotificacion = new System.Windows.Forms.Button();
+            this.pnlLogo = new System.Windows.Forms.Panel();
+            this.lblLogo = new System.Windows.Forms.Label();
+            this.pnlContenedor = new System.Windows.Forms.Panel();
+            this.PanelOpciones.SuspendLayout();
+            this.PanelMenu.SuspendLayout();
+            this.pnlLogo.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // PanelOpciones
+            // 
+            this.PanelOpciones.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("PanelOpciones.BackgroundImage")));
+            this.PanelOpciones.Controls.Add(this.btnHojaSocial);
+            this.PanelOpciones.Controls.Add(this.btnEnviarMensaje);
+            this.PanelOpciones.Controls.Add(this.btnEliminar);
+            this.PanelOpciones.Controls.Add(this.button3);
+            this.PanelOpciones.Controls.Add(this.btnDetalles);
+            this.PanelOpciones.Controls.Add(this.btnInicio);
+            this.PanelOpciones.Controls.Add(this.btnCerrarSesion);
+            this.PanelOpciones.Dock = System.Windows.Forms.DockStyle.Left;
+            this.PanelOpciones.Location = new System.Drawing.Point(0, 0);
+            this.PanelOpciones.Name = "PanelOpciones";
+            this.PanelOpciones.Size = new System.Drawing.Size(224, 487);
+            this.PanelOpciones.TabIndex = 2;
+            // 
+            // btnHojaSocial
+            // 
+            this.btnHojaSocial.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHojaSocial.Image = ((System.Drawing.Image)(resources.GetObject("btnHojaSocial.Image")));
+            this.btnHojaSocial.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnHojaSocial.Location = new System.Drawing.Point(0, 344);
+            this.btnHojaSocial.Name = "btnHojaSocial";
+            this.btnHojaSocial.Size = new System.Drawing.Size(224, 40);
+            this.btnHojaSocial.TabIndex = 13;
+            this.btnHojaSocial.Text = "Hoja de Hora Social";
+            this.btnHojaSocial.UseVisualStyleBackColor = true;
+            // 
+            // btnEnviarMensaje
+            // 
+            this.btnEnviarMensaje.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEnviarMensaje.Image = ((System.Drawing.Image)(resources.GetObject("btnEnviarMensaje.Image")));
+            this.btnEnviarMensaje.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnEnviarMensaje.Location = new System.Drawing.Point(0, 304);
+            this.btnEnviarMensaje.Name = "btnEnviarMensaje";
+            this.btnEnviarMensaje.Size = new System.Drawing.Size(224, 40);
+            this.btnEnviarMensaje.TabIndex = 12;
+            this.btnEnviarMensaje.Text = "Enviar mensaje";
+            this.btnEnviarMensaje.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEliminar.Image = ((System.Drawing.Image)(resources.GetObject("btnEliminar.Image")));
+            this.btnEliminar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnEliminar.Location = new System.Drawing.Point(0, 264);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(224, 40);
+            this.btnEliminar.TabIndex = 11;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Image = ((System.Drawing.Image)(resources.GetObject("button3.Image")));
+            this.button3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button3.Location = new System.Drawing.Point(0, 224);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(224, 40);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Modificar";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // btnDetalles
+            // 
+            this.btnDetalles.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDetalles.Image = ((System.Drawing.Image)(resources.GetObject("btnDetalles.Image")));
+            this.btnDetalles.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnDetalles.Location = new System.Drawing.Point(0, 184);
+            this.btnDetalles.Name = "btnDetalles";
+            this.btnDetalles.Size = new System.Drawing.Size(224, 40);
+            this.btnDetalles.TabIndex = 9;
+            this.btnDetalles.Text = "Detalles ";
+            this.btnDetalles.UseVisualStyleBackColor = true;
+            // 
+            // btnInicio
+            // 
+            this.btnInicio.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInicio.Image = ((System.Drawing.Image)(resources.GetObject("btnInicio.Image")));
+            this.btnInicio.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnInicio.Location = new System.Drawing.Point(0, 144);
+            this.btnInicio.Name = "btnInicio";
+            this.btnInicio.Size = new System.Drawing.Size(224, 40);
+            this.btnInicio.TabIndex = 0;
+            this.btnInicio.Text = "Inicio";
+            this.btnInicio.UseVisualStyleBackColor = true;
+            this.btnInicio.Click += new System.EventHandler(this.btnInicio_Click);
+            // 
+            // btnCerrarSesion
+            // 
+            this.btnCerrarSesion.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btnCerrarSesion.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCerrarSesion.Image = ((System.Drawing.Image)(resources.GetObject("btnCerrarSesion.Image")));
+            this.btnCerrarSesion.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCerrarSesion.Location = new System.Drawing.Point(0, 447);
+            this.btnCerrarSesion.Name = "btnCerrarSesion";
+            this.btnCerrarSesion.Size = new System.Drawing.Size(224, 40);
+            this.btnCerrarSesion.TabIndex = 8;
+            this.btnCerrarSesion.Tag = "Cerrar Sesión";
+            this.btnCerrarSesion.Text = "Cerrar Sesión";
+            this.btnCerrarSesion.UseVisualStyleBackColor = true;
+            // 
+            // PanelMenu
+            // 
+            this.PanelMenu.Controls.Add(this.btnPerfil);
+            this.PanelMenu.Controls.Add(this.btnNotificacion);
+            this.PanelMenu.Controls.Add(this.pnlLogo);
+            this.PanelMenu.Dock = System.Windows.Forms.DockStyle.Top;
+            this.PanelMenu.Location = new System.Drawing.Point(224, 0);
+            this.PanelMenu.Name = "PanelMenu";
+            this.PanelMenu.Size = new System.Drawing.Size(639, 40);
+            this.PanelMenu.TabIndex = 3;
+            // 
+            // btnPerfil
+            // 
+            this.btnPerfil.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.btnPerfil.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPerfil.Image = ((System.Drawing.Image)(resources.GetObject("btnPerfil.Image")));
+            this.btnPerfil.Location = new System.Drawing.Point(566, 0);
+            this.btnPerfil.Name = "btnPerfil";
+            this.btnPerfil.Size = new System.Drawing.Size(72, 40);
+            this.btnPerfil.TabIndex = 4;
+            this.btnPerfil.UseVisualStyleBackColor = true;
+            // 
+            // btnNotificacion
+            // 
+            this.btnNotificacion.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.btnNotificacion.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNotificacion.Image = ((System.Drawing.Image)(resources.GetObject("btnNotificacion.Image")));
+            this.btnNotificacion.Location = new System.Drawing.Point(494, 0);
+            this.btnNotificacion.Name = "btnNotificacion";
+            this.btnNotificacion.Size = new System.Drawing.Size(75, 40);
+            this.btnNotificacion.TabIndex = 3;
+            this.btnNotificacion.UseVisualStyleBackColor = true;
+            // 
+            // pnlLogo
+            // 
+            this.pnlLogo.Controls.Add(this.lblLogo);
+            this.pnlLogo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlLogo.Location = new System.Drawing.Point(0, 0);
+            this.pnlLogo.Name = "pnlLogo";
+            this.pnlLogo.Size = new System.Drawing.Size(639, 40);
+            this.pnlLogo.TabIndex = 2;
+            // 
+            // lblLogo
+            // 
+            this.lblLogo.AutoSize = true;
+            this.lblLogo.Font = new System.Drawing.Font("Times New Roman", 12.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLogo.Location = new System.Drawing.Point(16, 8);
+            this.lblLogo.Name = "lblLogo";
+            this.lblLogo.Size = new System.Drawing.Size(316, 19);
+            this.lblLogo.TabIndex = 0;
+            this.lblLogo.Text = "Sistema administrativo de Horas Sociales\r\n";
+            // 
+            // pnlContenedor
+            // 
+            this.pnlContenedor.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlContenedor.Location = new System.Drawing.Point(224, 40);
+            this.pnlContenedor.Name = "pnlContenedor";
+            this.pnlContenedor.Size = new System.Drawing.Size(639, 447);
+            this.pnlContenedor.TabIndex = 4;
+            // 
+            // FrmSisAd
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(863, 487);
+            this.Controls.Add(this.pnlContenedor);
+            this.Controls.Add(this.PanelMenu);
+            this.Controls.Add(this.PanelOpciones);
+            this.Location = new System.Drawing.Point(224, 40);
+            this.Name = "FrmSisAd";
+            this.Text = "Menú Administrativo";
+            this.PanelOpciones.ResumeLayout(false);
+            this.PanelMenu.ResumeLayout(false);
+            this.pnlLogo.ResumeLayout(false);
+            this.pnlLogo.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel PanelOpciones;
+        private System.Windows.Forms.Button btnCerrarSesion;
+        private System.Windows.Forms.Panel PanelMenu;
+        private System.Windows.Forms.Button btnPerfil;
+        private System.Windows.Forms.Button btnNotificacion;
+        private System.Windows.Forms.Panel pnlLogo;
+        private System.Windows.Forms.Label lblLogo;
+        private System.Windows.Forms.Panel pnlContenedor;
+        private System.Windows.Forms.Button btnInicio;
+        private System.Windows.Forms.Button btnHojaSocial;
+        private System.Windows.Forms.Button btnEnviarMensaje;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btnDetalles;
+    }
+}
+
---
+++ SistemaAdministrativo/FrmInicio.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaAdministrativo
+{
+    public partial class FrmSisAd : Form
+    {
+        public FrmSisAd()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void AbrirForm(object FrmInicio)
+        {
+            if(pnlContenedor.Controls.Count > 0)
+            {
+                this.pnlContenedor.Controls.RemoveAt(0);
+            }
+
+            Form fh = FrmInicio as Form;
+            fh.TopLevel = false;
+            fh.Dock = DockStyle.Fill;
+            this.pnlContenedor.Controls.Add(fh);
+            this.pnlContenedor.Tag = fh;
+            fh.Show();
+        }
+        private void btnInicio_Click(object sender, EventArgs e)
+        {
+            FrmDatosInicio us = new FrmDatosInicio();
+            AbrirForm(us);
+        }
+    }
+}
